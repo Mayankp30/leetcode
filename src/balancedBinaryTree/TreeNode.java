71e5a1b1773aea82169473510767b712420be1ed
Mayank Purohit <mayankpurohit@outlook.com>
2016-09-28 00:52:58 -0400

    Balanced Binary Tree

---
+++ src/balancedBinaryTree/TreeNode.java
@@ -0,0 +1,15 @@
+package balancedBinaryTree;
+
+/**
+ * Created by Mayank on 9/28/16.
+ */
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
