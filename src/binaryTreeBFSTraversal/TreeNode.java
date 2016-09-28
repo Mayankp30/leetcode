4360686d673d068f6025fc8c705654fa1d02ef68
Mayank Purohit <mayankpurohit@outlook.com>
2016-09-28 00:58:18 -0400

    BFS Traversal

---
+++ src/binaryTreeBFSTraversal/TreeNode.java
@@ -0,0 +1,12 @@
+package binaryTreeBFSTraversal;
+
+/**
+ * Created by Mayank on 9/28/16.
+ */
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
