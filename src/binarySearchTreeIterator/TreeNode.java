79bb5733d08678131db5f689f68e5cd735678e02
Mayank Purohit <mayankpurohit@outlook.com>
2016-09-28 00:56:37 -0400

    Binary Search Tree Iterator

---
+++ src/binarySearchTreeIterator/TreeNode.java
@@ -0,0 +1,14 @@
+package binarySearchTreeIterator;
+
+/**
+ * Created by Mayank on 9/28/16.
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
