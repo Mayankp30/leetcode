6b38b6b0dfb5c24c01b5e836552e7bb615c11813
Mayank Purohit <mayankpurohit@outlook.com>
2016-09-29 00:37:37 -0400

    kth largest/smallest array updated

--- src/kthLargest/KLargest.java
+++ src/kthLargest/KLargest.java
@@ -9,18 +9,22 @@ import java.util.HashSet;
 public class KLargest {
 
     public int kthLargest(int[] array, int k){  // 12, 3, 5, 6, 1, 3, 10, 14, 3, 10, 5, 22, 1, 0, -5, 10, 4, -10
-        HashSet<Integer> setOfUniqueNumbers = new HashSet<>();
+        HashSet<Integer> setOfUniqueNumbers = new HashSet<>(); // set to remove duplicate elements
         for (int value : array){
             setOfUniqueNumbers.add(value);
         }
 
-        int[] result = new int[setOfUniqueNumbers.size()];
+        int[] result = new int[setOfUniqueNumbers.size()];  // adding elements to array
         int element=0;
         for (int valueFromSet : setOfUniqueNumbers){
             result[element++]=valueFromSet;
         }
+
         Arrays.sort(result);
-        return result[result.length-k];
+        // return result[k-1];   smallest
+        return result[result.length-k]; // largest
+
+
 
     }
 }
