6b2a67dca693da385127e52d87e5f9f16a5ee592
Mayank Purohit <mayankpurohit@outlook.com>
2016-10-02 23:29:19 -0400

    Three Sum updated

--- src/threeSum/ThreeSum.java
+++ src/threeSum/ThreeSum.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**********************************************************************************
+ *
+ *
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ *
+ * For example, given array S = {-1 0 1 2 -1 -4},
+ * A solution set is:
+ * (-1, 0, 1)
+ * (-1, -1, 2)
+ * 
+ **********************************************************************************/
+
+
+
+
 
 /**
  * Created by Mayank on 10/2/16.
