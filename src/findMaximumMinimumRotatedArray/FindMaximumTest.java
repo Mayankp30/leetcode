d6b5b934b134a1af902f84436176ce1f6e238fe4
Mayank Purohit <mayankpurohit@outlook.com>
2016-10-06 23:41:33 -0400

    find maximum and minimum in rotated sorted array

---
+++ src/findMaximumMinimumRotatedArray/FindMaximumTest.java
@@ -0,0 +1,16 @@
+package findMaximumMinimumRotatedArray;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mayank on 10/6/16.
+ */
+public class FindMaximumTest {
+    @Test
+    public void findMaximum() throws Exception {
+
+    }
+
+}
