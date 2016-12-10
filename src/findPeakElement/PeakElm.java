6db8dc915b3d6c5247a0d0a7c40f4f4d17d89d36
Mayank Purohit <mayankpurohit@outlook.com>
2016-12-09 23:24:16 -0500

    Peak Element

--- src/findPeakElement/PeakElm.java
+++ src/findPeakElement/PeakElm.java
@@ -26,6 +26,32 @@ package findPeakElement;
 
 class PeakElm{
 
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid - 1] < nums[mid]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] > nums[end]) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
